b196b62f4fafbf3ffe0f96c54975100f472f11e6
Pablo <pablowoina2205@gmail.com>
2021-02-17 10:14:57 -0300

    Python OO Parte 2 - first commit

---
+++ Python OO 2/modelo.py
@@ -0,0 +1,19 @@
+class Filme:
+
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = nome
+        self.duracao = duracao
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
+
+
+vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
+print(vingadores.nome)
+
+atlanta = Serie("Atlanta", 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
