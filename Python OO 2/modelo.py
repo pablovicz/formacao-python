e23140a4818cf8f16822e2be43e708c1509c0de4
pablo.wramos <pablo.wramos@telefonica.com>
2021-02-17 15:58:30 -0300

    Removendo duplicação de código com herança

--- Python OO 2/modelo.py
+++ Python OO 2/modelo.py
@@ -1,48 +1,37 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome
+        self.ano = ano
+        self._likes = 0
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+
+class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    @property
-    def likes(self):
-        return self.__likes
 
 
-class Serie:
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    @property
-    def likes(self):
-        return self.__likes
 
 
 
@@ -56,4 +45,3 @@ atlanta.dar_like()
 atlanta.dar_like()
 
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
-
