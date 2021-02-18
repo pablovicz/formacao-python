44ef5b39926d74826fcd939f2ecd03d8a06063e7
pablo.wramos <pablo.wramos@telefonica.com>
2021-02-18 09:06:10 -0300

    Quando não usar herança

--- Python OO 2/modelo.py
+++ Python OO 2/modelo.py
@@ -1,6 +1,6 @@
 class Programa:
     def __init__(self, nome, ano):
-        self._nome = nome
+        self._nome = nome.title()
         self.ano = ano
         self._likes = 0
 
@@ -43,25 +43,52 @@ class Serie(Programa):
         return f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} temporadas - Likes: {self._likes}'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome.title()
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 
 
 vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
+vingadores.dar_like()
+vingadores.dar_like()
+tmep = Filme("todo mundo em pânico", 1999, 100)
+tmep.dar_like()
+tmep.dar_like()
 atlanta = Serie("Atlanta", 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
+atlanta.dar_like()
+demolidor = Serie("Demolidor", 2016, 2)
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 
 
 
-filmes_e_series = [vingadores, atlanta]
 
-for programa in filmes_e_series:
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
+
+print(f'tamanho da playlist: {playlist_fim_de_semana.tamanho}')
+
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
 
-
-
-
-
-
+print(f'Tá ou nao tá? {demolidor in playlist_fim_de_semana.listagem}')
 
 
 
