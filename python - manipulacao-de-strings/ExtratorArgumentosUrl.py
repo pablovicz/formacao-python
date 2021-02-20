6ad22fd5af2a18b1f866ab36c81576dffd98041e
Pablo <pablowoina2205@gmail.com>
2021-02-20 16:58:34 -0300

    Mais sobre métodos de strings

--- python - manipulacao-de-strings/ExtratorArgumentosUrl.py
+++ python - manipulacao-de-strings/ExtratorArgumentosUrl.py
@@ -1,13 +1,13 @@
 class ExtratorArgumentosUrl:
     def __init__(self, url):
         if self.url_eh_valida(url):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("Url Inválida!!")
 
     @staticmethod
     def url_eh_valida(url):
-        if url:
+        if url and url.startswith("https://bytebank.com/"):
             return True
         else:
             return False
@@ -15,7 +15,12 @@ class ExtratorArgumentosUrl:
     def extrai_argumentos(self):
 
         moeda_origem = self.cut_string("moedadestino=")
+
         moeda_destino = self.cut_string("moedaorigem=")
+
+        if moeda_origem == moeda_destino:
+            self.troca_moeda_origem(self)
+
         valor = self.split_string("valor=")
 
         return moeda_origem, moeda_destino, valor
@@ -28,3 +33,9 @@ class ExtratorArgumentosUrl:
         moeda = self.split_string(moeda_buscada)
 
         return moeda[:moeda.find("&")]
+
+    def troca_moeda_origem(self):
+        self.url = self.url.replace("moedadestino", "real")
+        print(self.url)
+
+
