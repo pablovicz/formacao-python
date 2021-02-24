14cd599673434af6c900317206d7f1aaecf011ac
pablo.wramos <pablo.wramos@telefonica.com>
2021-02-24 11:48:45 -0300

    Ordem Natural

--- Parte 1 - Listas e Tuplas/OutrosBultIns.py
+++ Parte 1 - Listas e Tuplas/OutrosBultIns.py
@@ -1,4 +1,4 @@
-idades = [21,45, 34, 65, 25, 57, 35]
+idades = [21, 45, 34, 65, 25, 57, 35]
 
 for i in range(len(idades)):
     print(i, idades[i])
@@ -8,6 +8,16 @@ for i in range(len(idades)):
 for idade in enumerate(idades):
     print(idade)
 
+
+## Sorted - ordena in place
+print(sorted(idades)) # crescente
+
+print(sorted(idades, reverse=True)) # descendente
+
+
+## Sort
+idades.sort()  # vai ordenar o objeto lista
+print(idades)
 
 
 
