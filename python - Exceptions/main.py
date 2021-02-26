f5e8a3497caeb4f25bc771ca08f5cd9cec6b9982
pablo.wramos <pablo.wramos@telefonica.com>
2021-02-26 17:14:17 -0300

    Propagacao e Disparo de Exceções

--- python - Exceptions/main.py
+++ python - Exceptions/main.py
@@ -2,7 +2,13 @@
 ## try /  except
 
 def dividir(dividendo, divisor):
-    return dividendo / divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError("dividir() deve receber apenas argumentos inteiros")
+    try:
+        return dividendo // divisor
+    except:
+        print(f'Não foi possível dividir {dividendo} por {divisor}')
+        raise
 
 def testa_divisao(divisor):
     resultado = ""
@@ -13,15 +19,18 @@ def testa_divisao(divisor):
         if resultado != "":
             print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
 
-    except ZeroDivisionError:
+    except ZeroDivisionError as E:
+        print(E.__class__.__bases__) ## printa classe pai da exception
         raise ZeroDivisionError('Erro de divisão por 0 tratado')
-    except AttributeError:
+    except AttributeError as E:
+        print(E.__class__.__bases__)
         raise AttributeError('Erro de atributo tratado')
+#    except Exception:
+#        raise Exception('Erro genérico') # caso nenhum dos anteriores seja indenficado, faz o traatamento generico de erros
 
+## https://docs.python.org/3/library/exceptions.html
 
-
-
-testa_divisao(0)
+testa_divisao(5.8)
 
 
 
