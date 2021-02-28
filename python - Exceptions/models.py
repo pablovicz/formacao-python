f1d36396d30c845cdd610e3ecef6fb84b36015ab
Pablo <pablowoina2205@gmail.com>
2021-02-28 17:29:21 -0300

    Argumentos e Alias

--- python - Exceptions/models.py
+++ python - Exceptions/models.py
@@ -9,12 +9,52 @@ class  ContaCorrente:
     taxa_operacao = None
 
     def __init__(self, cliente, agencia, numero):
-        self.saldo  = 100
+        self.__saldo = 100
+        self.__agencia = 0
+        self.__numero = 0
+
         self.cliente = cliente
-        self.agencia = agencia
-        self.numero = numero
+        self.__set_agencia(agencia)
+        self.__set_numero(numero)
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    @property
+    def agencia(self):
+        return self.__agencia
+
+    def __set_agencia(self, value):
+        if not isinstance(value, int):
+            raise ValueError("O atributo agência deve ser um valor inteiro")
+        elif value <= 0:
+            raise ValueError("O atributo agência deve ser um valor maior que zero")
+
+        self.__agencia = value
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    def __set_numero(self, value):
+        if not isinstance(value, int):
+            raise ValueError("O atributo número deve ser um valor inteiro")
+        elif value <= 0:
+            raise ValueError("O atributo número deve ser um valor maior que zero")
+
+        self.__numero = value
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, value):
+        if not isinstance(value, int):
+            raise ValueError("O atributo saldo deve ser um valor inteiro")
+        elif value <= 0:
+            raise ValueError("O atributo saldo deve ser um valor maior que zero")
+
+        self.__saldo = value
 
     def transferir(self, valor, favorecido):
         favorecido.depositar(valor)
