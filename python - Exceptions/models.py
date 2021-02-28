3dbbb4f5d1f498495fbb9291d2bcc9e80ce27b19
pablo.wramos <pablo.wramos@telefonica.com>
2021-02-28 16:41:46 -0300

    python exceptions

---
+++ python - Exceptions/models.py
@@ -0,0 +1,27 @@
+class Cliente:
+    def __init__(self, nome, cpf, profissao):
+        self.nome = nome
+        self.cpf  = cpf
+        self.profissao = profissao
+
+class  ContaCorrente:
+    total_contas_criadas = 0
+    taxa_operacao = None
+
+    def __init__(self, cliente, agencia, numero):
+        self.saldo  = 100
+        self.cliente = cliente
+        self.agencia = agencia
+        self.numero = numero
+        ContaCorrente.total_contas_criadas += 1
+        ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    def transferir(self, valor, favorecido):
+        favorecido.depositar(valor)
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+    def depositar(self, valor):
+        self.saldo += valor
+
