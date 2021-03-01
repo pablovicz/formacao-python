998e668f6388c4be5c6f65ea8fba5edbe0c2ddab
pablo.wramos <pablo.wramos@telefonica.com>
2021-03-01 11:20:57 -0300

    Template Method

--- python - design patterns/Design Patterns - Parte 1/calculador_de_impostos.py
+++ python - design patterns/Design Patterns - Parte 1/calculador_de_impostos.py
@@ -1,4 +1,4 @@
-from impostos import ISS, ICMS
+from impostos import ISS, ICMS, ICPP, IKCV
 
 
 class CalculadorDeImpostos(object):
@@ -7,15 +7,26 @@ class CalculadorDeImpostos(object):
         ## Design Patter Strategy - passando as funcoes de calculo de imposto como parametro para o método
         imposto_calculado = imposto.calcula(orcamento)
 
-        print(imposto_calculado)
+        print("R$", "{0:.2f}".format(imposto_calculado))
+
 
 
 if __name__ == '__main__':
-    from orcamento import Orcamento
+    from orcamento import Orcamento, Item
 
     calculador = CalculadorDeImpostos()
 
-    orcamento = Orcamento(500)
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('Item - 1', 100))
+    orcamento.adiciona_item(Item('Item - 2', 50))
+    orcamento.adiciona_item(Item('Item - 3', 400))
 
+
+    print(f'ISS:')
     calculador.realiza_calculo(orcamento, ISS())
+    print(f'ICMS:')
     calculador.realiza_calculo(orcamento, ICMS())
+    print(f'ICPP:')
+    calculador.realiza_calculo(orcamento, ICPP())
+    print(f'IKCV:')
+    calculador.realiza_calculo(orcamento, IKCV())
