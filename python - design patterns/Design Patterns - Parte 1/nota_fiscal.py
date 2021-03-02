3e2b218e5661f17c9c68c0c022030ed27af822d9
pablo.wramos <pablo.wramos@telefonica.com>
2021-03-02 11:11:08 -0300

    Criacao de Objetos e Builder

--- python - design patterns/Design Patterns - Parte 1/nota_fiscal.py
+++ python - design patterns/Design Patterns - Parte 1/nota_fiscal.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 # nota_fiscal.py
 from datetime import date
-from criador_de_nota_fiscal import BuilderDeNotaFiscal
 
 class Item(object):
 
@@ -19,7 +18,7 @@ class Item(object):
 
 class NotaFiscal(object):
 
-    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes=''): ##parametros opcionais vem por ultimo
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes='', observadores=[]): ##parametros opcionais vem por ultimo
         self.__razao_social = razao_social
         self.__cnpj = cnpj
         self.__data_de_emissao = data_de_emissao
@@ -27,6 +26,12 @@ class NotaFiscal(object):
             raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
         self.__detalhes = detalhes
         self.__itens = itens
+
+        ##Design Pattern - Observer
+        for observador in observadores:
+            observador(self)
+
+
 
     @property
     def razao_social(self):
@@ -48,6 +53,9 @@ class NotaFiscal(object):
     # código das classes omitidos
 
 if __name__ == '__main__':
+    from criador_de_nota_fiscal import BuilderDeNotaFiscal
+    from observadores import imprime, envia_por_email, salva_no_banco
+
     itens = [
        Item(
             'ITEM A',
@@ -63,10 +71,13 @@ if __name__ == '__main__':
         razao_social='FHSA Limitada',
         cnpj='012345678901234',
         itens=itens,
+        observadores=[imprime, envia_por_email, salva_no_banco]
     )
 
+'''
     nota_fiscal_com_builder = (BuilderDeNotaFiscal()
                                .com_razao_social('FHSA Limitada')
                                .com_cnpj('012345678901234')
-                               .com_itens(itens))
-
+                               .com_itens(itens)
+                               .build())
+'''
