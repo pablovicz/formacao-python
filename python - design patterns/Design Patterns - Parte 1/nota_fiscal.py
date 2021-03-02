cb19c20daa19fbe2222b615f12c4e019127f66e6
pablo.wramos <pablo.wramos@telefonica.com>
2021-03-02 10:23:01 -0300

    Criacao de Objetos e Builder

---
+++ python - design patterns/Design Patterns - Parte 1/nota_fiscal.py
@@ -0,0 +1,72 @@
+# -*- coding: UTF-8 -*-
+# nota_fiscal.py
+from datetime import date
+from criador_de_nota_fiscal import BuilderDeNotaFiscal
+
+class Item(object):
+
+    def __init__(self, descricao, valor):
+        self.__descricao = descricao
+        self.__valor = valor
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @property
+    def valor(self):
+        return self.__valor
+
+class NotaFiscal(object):
+
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes=''): ##parametros opcionais vem por ultimo
+        self.__razao_social = razao_social
+        self.__cnpj = cnpj
+        self.__data_de_emissao = data_de_emissao
+        if len(detalhes) > 20:
+            raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
+        self.__detalhes = detalhes
+        self.__itens = itens
+
+    @property
+    def razao_social(self):
+        return self.__razao_social
+
+    @property
+    def cnpj(self):
+        return self.__cnpj
+
+    @property
+    def data_de_emissao(self):
+        return self.__data_de_emissao
+
+    @property
+    def detalhes(self):
+        return self.__detalhes
+
+    # nota_fiscal.py
+    # código das classes omitidos
+
+if __name__ == '__main__':
+    itens = [
+       Item(
+            'ITEM A',
+            100
+       ),
+       Item(
+            'ITEM B',
+            200
+       )
+    ]
+
+    nota_fiscal = NotaFiscal(
+        razao_social='FHSA Limitada',
+        cnpj='012345678901234',
+        itens=itens,
+    )
+
+    nota_fiscal_com_builder = (BuilderDeNotaFiscal()
+                               .com_razao_social('FHSA Limitada')
+                               .com_cnpj('012345678901234')
+                               .com_itens(itens))
+
