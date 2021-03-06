ceb26f29e9303e8bf287d4598c44d074b2e27681
Pablo <pablowoina2205@gmail.com>
2021-03-06 16:13:11 -0300

    DSLs e Design Pattern Interpreter

---
+++ python - design patterns/Design Patterns - Parte 2/operacoes.py
@@ -0,0 +1,43 @@
+#Dsl - domain specific language
+# Design Pattern - Interpreter
+
+
+class Subtracao(object):
+
+    def __init__(self, espressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = espressao_esquerda
+        self.__expressao_direita = expressao_direita
+
+    def avalia(self):
+        return self.__expressao_esquerda.avalia() - self.__expressao_direita.avalia()
+
+
+class Soma(object):
+
+    def __init__(self, espressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = espressao_esquerda
+        self.__expressao_direita = expressao_direita
+
+    def avalia(self):
+        return self.__expressao_esquerda.avalia() + self.__expressao_direita.avalia()
+
+
+class Numero(object):
+
+    def __init__(self, numero):
+        self.__numero = numero
+
+    def avalia(self):
+        return self.__numero
+
+if __name__ == '__main__':
+    expressao_esquerda = Soma(Numero(10), Numero(20))
+    expressao_direita  =Soma(Numero(5), Numero(2))
+    expressao_conta = Soma(expressao_esquerda, expressao_direita)
+
+    print(expressao_conta.avalia())
+
+    expressao_conta_2 = Subtracao(Numero(100), Numero(70))
+    print(expressao_conta_2.avalia())
+
+
