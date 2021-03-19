fa23c2606e939a4085b14b2c3ba099f5e98e0774
Pablo <pablowoina2205@gmail.com>
2021-03-19 07:59:24 -0300

    Testando excecoes

--- python - testes automatizados/src/leilao/test_leilao.py
+++ python - testes automatizados/src/leilao/test_leilao.py
@@ -65,3 +65,37 @@ class TestLeilao(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    # se leilao nao tiber lances, deve permitir propor um lance
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_do_gui)
+        quantidade_de_lances_recebida = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebida)
+
+    # se o ultmo usuario for diferente, deve permitir propor o lance
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 200.00)
+            self.leilao.propoe(lance_do_yuri)
+            self.leilao.propoe(self.lance_do_gui)
+
+            quantidade_de_lances_recebido = len(self.leilao.lances)
+
+            self.assertEqual(2, quantidade_de_lances_recebido)
+
+
+    # se o ultimo usuario for o mesmo, nao deve permitir propor o lance
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_de_gui200 = Lance(self.gui, 200.00)
+
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(lance_de_gui200)
+            self.leilao.propoe(self.lance_do_gui)
+
+
+
+
+
+
